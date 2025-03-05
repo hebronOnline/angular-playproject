144274c6dad697de4735bbc4f0acde76f55661e3
Hebron Ncabane <muzi.hebron@gmail.com>
2025-03-05 15:57:45 +0200

    Added user tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {UserComponent} from './user/user.component';
+import {DUMMY_USERS} from './user/dummy-users';
+import {TasksComponent} from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'course-app';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+
+  get userName() {
+    return this.users.find((user) => user.id == this.selectedUserId)?.name
+  }
+
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import  { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  selector: 'app-header',
+  standalone: true,
+})
+export class HeaderComponent {}
